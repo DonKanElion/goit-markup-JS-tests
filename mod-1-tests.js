3f53cccc38f7ba9273d1ad9408a97a40aa077e9b
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-06-03 23:20:58 +0300

    Автоперевірка мод 1 завершена.

--- mod-1-tests.js
+++ mod-1-tests.js
@@ -317,4 +317,257 @@ console.log('Mod 1 tests');
 //   Виклик isNumberNotInRange(10, 30, 17) повертає false
 // Виклик isNumberNotInRange(10, 30, 5) повертає true
 
-// 24 
+// 24 ЗАДАЧА: ОБЧИСЛЕННЯ ЗНИЖКИ. Функція getDiscount(totalSpent) визначає значення знижки, залежно від загальної суми витрачених грошей (параметр totalSpent) в магазині за весь час...
+
+// function getDiscount(totalSpent) {
+//     const BASE_DISCOUNT = 0;
+//     const BRONZE_DISCOUNT = 0.02;
+//     const SILVER_DISCOUNT = 0.05;
+//     const GOLD_DISCOUNT = 0.1;
+//     let discount;
+//     // Change code below this line
+//     if (totalSpent >= 50000){
+//         discount = GOLD_DISCOUNT;
+//     }
+//     else if (totalSpent >= 20000 && totalSpent <= 49999){
+//     discount = SILVER_DISCOUNT;
+//     }
+//     else if (totalSpent >= 5000 && totalSpent <= 19999){
+//         discount = BRONZE_DISCOUNT;
+//     }
+//     else{
+//         discount = BASE_DISCOUNT;
+//     }
+//     // Change code above this line
+//     return discount;
+//   }
+  
+//   check:
+//   Виклик getDiscount(137000) повертає 0.1
+//   Виклик getDiscount(46900) повертає 0.05
+//   Виклик getDiscount(8250) повертає 0.02
+
+  // 25 ТЕРНАРНИЙ ОПЕРАТОР Виконай рефакторинг рішення задачі «Склад товарів», замінивши інструкцію if...else тернарним оператором.
+
+//   function checkStorage(available, ordered) {
+//     let message;
+//     // Change code below this line
+  
+//     // if (ordered > available) {
+//     //   message = "Not enough goods in stock!";
+//     // } else {
+//     //   message = "The order is accepted, our manager will contact you";
+//     // }
+
+ 
+  
+//     // Change code above this line  
+
+//     message = ordered > available ? "Not enough goods in stock!" : "The order is accepted, our manager will contact you";
+
+//     return message;
+//   }
+
+
+//   Check:
+//   Виклик checkStorage(100, 130) повертає "Not enough goods in stock!"
+// Виклик checkStorage(200, 20) повертає "The order is accepted, our manager will contact you"
+
+// 26 ЗАДАЧА: ПЕРЕВІРКА ПАРОЛЯ. Функція checkPassword(password) порівнює переданий їй пароль (параметр password) зі збереженим ....
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+// message = password === "jqueryismyjam" ? "Access is allowed" : "Access denied, wrong password!";
+//   // Change code above this line
+//   return message;
+// }
+
+
+//27 ІНСТРУКЦІЯ SWITCH. Функція getSubscriptionPrice(type) отримує рядок з типом передплати користувача (параметр type), перевіряє її.....
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//  switch (type) { // Change this line
+//     case "organization" : // Change this line
+//       price = 50; // Change this line
+//       break;
+
+//     case "professional": // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case "starter" : // Change this line
+//       price = 0 ; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+
+// 28. ІНСТРУКЦІЯ SWITCH З БЛОКОМ DEFAULT. Функція checkPassword(password) отримує пароль в параметр password, перевіряє його на збіг з паролем адміністратора ....
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+
+//   switch (password) {
+//     case null:
+//     message = "Canceled by user!";
+//     break;
+//      case ADMIN_PASSWORD: 
+//     message = "Welcome!";
+//     break;
+//    default: 
+//     message = "Access denied, wrong password!";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// check:
+// Виклик checkPassword(null) повертає "Canceled by user!"
+// Виклик checkPassword("polyhax") повертає "Access denied, wrong password!"
+
+//29. ЗАДАЧА: ДОСТАВКА ТОВАРУ. Функція getShippingCost(country) повинна перевіряти можливість доставки товару в країну користувача (параметр country) і повертати повідомлення про результат...
+
+// function getShippingCost(country) {
+//   let message;
+//   // Change code below this line
+//   let price; 
+
+//   switch (country){
+    
+//       case "China":
+//         price = 100;
+//         message = `Shipping to ${country} will cost ${price} credits`;
+//         break;    
+
+//       case "Chile":
+//         price = 250;
+//         message = `Shipping to ${country} will cost ${price} credits`;
+//         break;
+
+//       case "Australia":
+//         price = 170;
+//         message = `Shipping to ${country} will cost ${price} credits`;
+//         break;
+
+//       case "Jamaica":
+//       price = 120;
+//       message = `Shipping to ${country} will cost ${price} credits`;
+//       break;
+
+//       default:
+//         message = "Sorry, there is no delivery to your country";
+//   }
+//   // Change code above this line
+//   return message;
+// }
+
+// Check: 
+
+// Виклик getShippingCost("Australia") повертає "Shipping to Australia will cost 170 credits"
+// Виклик getShippingCost("Germany") повертає "Sorry, there is no delivery to your country"
+
+
+// 30. ДОВЖИНА РЯДКА. Функція getNameLength(name) приймає ім'я (параметр name) і повертає рядок, в якому вказана його довжина. Доповни шаблонний рядок у змінній message довжиною рядка з параметра name.
+
+// function getNameLength(name) {
+//   const message = `Name ${name} is ${name.length} characters long`; // Change this line
+
+//   return message;
+// }
+
+
+// 31. ІНДЕКСАЦІЯ ЕЛЕМЕНТІВ РЯДКА. 
+
+
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length -1];
+
+// // Change code above this line
+
+// 32. МЕТОД SLICE(). Функція getSubstring(string, length) приймає рядок і повертає підрядок від початку і до length символів. Вона оголошує два параметри, значення яких будуть задаватися під час її виклику:
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length); // Change this line
+
+//   return substring;
+// }
+
+// Check:
+// Виклик функції getSubstring("Hello world", 3) повертає "Hel"
+// Виклик функції getSubstring("Hello world", 6) повертає "Hello"
+
+
+// 33. ЗАДАЧА: ФОРМАТУВАННЯ ПОВІДОМЛЕННЯ. Функція formatMessage(message, maxLength) приймає рядок (параметр message) і форматує його, якщо довжина перевищує значення в параметрі maxLength.
+
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Change code below this line
+
+//   // if (message.length >= maxLength){
+//   //   result = message.slice(0, maxLength) + "...";
+//   // }
+//   // else{
+//   //   result = message;
+//   // }
+
+//   // Короткий варівант.
+   
+//   result = message.length > maxLength ? message.slice(0, maxLength) + "..." : message;
+
+//   /// Change code above this line
+//   return result;
+// }
+
+// Check: 
+// Виклик функції formatMessage("Curabitur ligula sapien", 16) повертає "Curabitur ligula..."
+// Виклик функції formatMessage("Curabitur ligula sapien", 23) повертає "Curabitur ligula sapien"
+
+// 34. МЕТОДИ TOLOWERCASE() І TOUPPERCASE().  Функція normalizeInput(input) приймає рядок (параметр input) і повертає такий самий рядок, але в нижньому регістрі. Присвой змінній normalizedInput вираз створення рядка у нижньому регістрі з параметра input.
+
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase() ; // Change this line
+
+//   return normalizedInput;
+// }
+
+
+// 35. МЕТОД INCLUDES(). Функція checkName(fullname, name) приймає два параметри та повертає буль true або false - результат перевірки входження підрядка name у рядок fullname.
+
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name); // Change this line
+//    return result;
+//  }
+
+ // 36. ЗАДАЧА: ПЕРЕВІРКА СПАМУ. Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути у довільному регістрі, наприклад SPAM або sAlE.
+
+//  function checkForSpam(message) {
+//   let result;
+//   // Change code below this line
+
+//   const normalizedInput = message.toLowerCase();
+//   result = normalizedInput.includes("spam") ||  normalizedInput.includes("sale");
+
+//   // Change code above this line
+//   return result;
+// }
+
+//  check:
+
+//  Виклик функції checkForSpam("Latest technology news") повертає false
+// Виклик функції checkForSpam("JavaScript weekly newsletter")повертає false
+
+// END === УРАААА!!!!
