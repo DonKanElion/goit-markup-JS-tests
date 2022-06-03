2e4e1658c9c6f0582093605ee397909ce1bce7e4
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-06-04 00:19:22 +0300

    Репета МД-2-01(задачки)

---
+++ 06-transform.js
@@ -0,0 +1,40 @@
+console.log("06. Об'єднання всіх елементів в один рядок!"); 
+
+
+/*
+ * Напиши скрипт, который объединяет все элементы массива в одно строковое значение.
+ * Элементов может быть произвольное кол-во.
+ * Пусть элементы массива  в строке будут разделены запятой.
+ * - Сначала через for
+ * - Потом через join()
+ */
+
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+
+// const string = friends.join(", ");
+// console.log(friends); // 'Mango', 'Poly', 'Kiwi', 'Ajax']
+// console.log(string); // Mango, Poly, Kiwi, Ajax
+
+// ==========
+
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// let string = '';
+
+// for (const friend of friends) {
+//   string += friend + ',';
+// }
+
+// // string = string.slice(0, string.length - 1);
+
+// const string = friends.join(',');
+// console.log(friends);
+// console.log(string);
+
+// // Должно получиться 'Mango,Poly,Kiwi,Ajax'
+// © 2022 GitHub, Inc.
+// Terms
+// Privacy
+// Security
+// Status
+// Docs
+
---
+++ 07-change-register.js
@@ -0,0 +1,33 @@
+console.log("07. Заміна реєстру на протилежний"); 
+
+   
+/*
+ * Напиши скрипт который заменяет регистр каждого символа в строке на противоположный.
+ * Например, если строка «JavaScript», то на выходе должена быть строка «jAVAsCRIPT».
+ */
+
+// const string = 'JavaScript';
+// const letters = string.split('');
+// let invertedString = '';
+
+// console.log(letters);
+
+// for (const letter of letters) {
+//   console.log(letter);
+
+//   // if (letter === letter.toLowerCase()) {
+//   //   console.log('Эта буква в нижнем регистре!!! - ', letter);
+
+//   //   invertedString += letter.toUpperCase();
+//   // } else {
+//   //   console.log('Эта буква в верхнет регистре!!! - ', letter);
+//   //   invertedString += letter.toLowerCase();
+//   // }
+
+// //   додаткова змінна для кращої читаємості:
+//   const isEqual = letter === letter.toLowerCase();
+// // Тернарний оператор (спрощення)
+//   invertedString += isEqual ? letter.toUpperCase() : letter.toLowerCase();
+// }
+
+// console.log('invertedString: ', invertedString);
